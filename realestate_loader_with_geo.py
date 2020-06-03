da68a173d13012f83dda0f683f9174c81f777942
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-06-03 16:45:04 +0100

    Fully working
    Note the new "FEEDS" format.
    
    Also good reference for using Yield with Scrapy!

--- realestate_loader_with_geo.py
+++ realestate_loader_with_geo.py
@@ -1,20 +1,21 @@
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 #|r|e|d|a|n|d|g|r|e|e|n|.|c|o|.|u|k|
 #+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
 # Craigslist Scrapy Project #
+''' Scrapes approx 3000 Properties on Craigslist
+    and gets geo data from details page '''
+    
 import os
 import scrapy
 from scrapy.loader import ItemLoader
 from scrapy.crawler import CrawlerProcess
-from items import CraigslistItem
-from urllib.parse import urljoin
-import requests
-import time
+from items import CraigslistItem # (same level as this spider )
 
 class RealestateSpider(scrapy.Spider):
 
     name = 'realestate_loader'
-    #allowed_domains = ['newyork.craigslist.org/d/real-estate/search/rea']
+
     start_urls = ['http://newyork.craigslist.org/d/real-estate/search/rea/']
     try:
         os.remove('results.csv')
@@ -32,37 +33,40 @@ class RealestateSpider(scrapy.Spider):
 
         all_ads = response.xpath('//p[@class="result-info"]')
         for ads in all_ads:
-            #details_link = ads.xpath(".//a[@class='result-title hdrlnk']/@href").getall()
+
+            # details_link = The Link to the Detail page that contains the geo data #
             details_link = ads.xpath(".//a[@class='result-title hdrlnk']/@href").get()
-            # get GEO data from details link #
-            #self.parse_detail(details_link)
+
+            # get GEO data from details link - and come back with geo data for loader #
             yield response.follow(url=details_link, callback=self.parse_detail)
-        #####################################
-        loader = ItemLoader(item=CraigslistItem(),selector = ads,response=response)
-        loader.add_xpath("price",".//span[@class='result-price']/text()")
-        loader.add_xpath("date",".//time[@class='result-date']/text()")
-        loader.add_xpath("title",".//a[@class='result-title hdrlnk']/text()")
-        loader.add_xpath("hood",".//span[@class='result-hood']/text()")
-        loader.add_xpath("details_link",".//a[@class='result-title hdrlnk']/@href")
-        loader.add_value("lon",self.lon)
-        loader.add_value("lat",self.lat)
-        yield loader.load_item()
-        #####################################
+
+            #
+            loader = ItemLoader(item=CraigslistItem(),selector=ads,response=response)
+            loader.add_xpath("price",".//span[@class='result-price']/text()")
+            loader.add_xpath("date",".//time[@class='result-date']/text()")
+            loader.add_xpath("title",".//a[@class='result-title hdrlnk']/text()")
+            loader.add_xpath("hood",".//span[@class='result-hood']/text()")
+            loader.add_xpath("details_link",".//a[@class='result-title hdrlnk']/@href")
+            loader.add_value("lon",self.lon)
+            loader.add_value("lat",self.lat)
+            yield loader.load_item()
+
+        # Get the next 25 properties from 'next page' - persist until no more #
         next_page = response.xpath("//a[@class='button next']/@href").get()
-        if next_page is not None:
+        if next_page:
             yield response.follow(url=next_page, callback=self.parse)
 
     # Drop down to details level to extract the Geo coordinates #
-    #<meta name="detail.position" content="40.578355;-73.959875">#
+    # <meta name="detail.position" content="40.578355;-73.959875"> #
     def parse_detail(self,response):
-
-        #yield scrapy.Request(details_link, callback = self.parse_list)
+        # called from : yield response.follow(url=details_link, callback=self.parse_detail)
         self.lon = response.xpath('//meta[@name="geo.position"]/@content').get().split(";")[0]
         self.lat = response.xpath('//meta[@name="geo.position"]/@content').get().split(";")[1]
+        # parse resumes at ItemLoader and fills containers, inc Lon, and Lat.
 
 # main driver #
 if __name__ == "__main__" :
-
+    # Create Instance called 'cl' as in "c"raigs "l"ist
     cl = CrawlerProcess(settings={
     "FEEDS": {
         "results.csv": {"format": "csv"},
